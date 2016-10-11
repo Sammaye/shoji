389e027669c766ae70e064666a88e4b4eca47227
Sam Millman <sam.millman@gmail.com>
2016-10-11 08:54:42 +0000

    An actual working example

--- shoji.js
+++ shoji.js
@@ -5,24 +5,38 @@
             var data = $.extend({}, $.fn.shoji.defaults, options);
             
             if(!$(this).data($.fn.shoji.defaults.data_index)){
+                
                 $(data['btn_open']).bind('click', function(e){
                     e.preventDefault();
-                    $.fn.shoji.open($(this))
-                });
+                    var shata = $(this).data($.fn.shoji.defaults.data_index);
+                    if(shata){
+                        $.fn.shoji.open(shata['menu_object']);
+                    }
+                }).data(
+                    $.fn.shoji.defaults.data_index, 
+                    {'menu_object': $this}
+                );
+                
                 $(data['btn_close']).bind('click', function(e){
                     e.preventDefault();
-                    $.fn.shoji.close($(this))
-                });
+                    var shata = $(this).data($.fn.shoji.defaults.data_index);
+                    if(shata){
+                        $.fn.shoji.close(shata['menu_object']);
+                    }
+                }).data(
+                    $.fn.shoji.defaults.data_index, 
+                    {'menu_object': $this}
+                );
                 
                 data['btn_open_object'] = $(data['btn_open']);
                 data['btn_close_object'] = $(data['btn_close']);
                 
-                $this
-                    .data($.fn.shoji.data_index, data)
-                    .addClass($.fn.shoji.menu);
+                $this.data($.fn.shoji.defaults.data_index, data);
+                $this.addClass($.fn.shoji.defaults.menu.replace('.', ''));
+                
+                $(window).trigger('resize');
             }
         });
-        $(window).trigger('resize');
     };
     $.fn.shoji.defaults = {
         breakpoints: {
@@ -33,10 +47,11 @@
             'xxs': 320
         },
         partial_breakpoint: 'md',
-        fullscreen_breakpoint: 'xs',
+        fullscreen_breakpoint: 'sm',
+        sliding_menu_class: 'shoji-menu-hidden',
         active_menu_class: 'shoji-menu-active',
         btn_open: '.shoji-open-menu',
-        btn_close: 'sohji-close-menu',
+        btn_close: '.shoji-close-menu',
         animate: true,
         animate_speed: 400,
         
@@ -55,21 +70,27 @@
         var lower_break = shata['breakpoints'][shata['fullscreen_breakpoint']];
         
         if(window.innerWidth <= upper_break && window.innerWidth > lower_break){
-            el
-                .css({left: '-2000px', display: 'block'})
-                .animate(
-                    {
-                        left: 0
-                    }, 
-                    {
-                        'duration': shata['animate_speed'],
-                        'complete': function(){
-                            $(document.body).css({overflow: 'hidden'});
+            if(shata['animate']){
+                el
+                    .addClass(shata['active_menu_class'])
+                    .css({left: '-2000px'})
+                    .animate(
+                        {
+                            left: 0
+                        }, 
+                        {
+                            'duration': shata['animate_speed'],
+                            'complete': function(){
+                                $(document.body).css({overflow: 'hidden'});
+                            }
                         }
-                    }
-                );
+                    );
+            }else{
+                el.addClass(shata['active_menu_class']);
+                $(document.body).css({overflow: 'hidden'});
+            }
         }else if(window.innerWidth <= lower_break){
-            el.css({display: 'block'});
+            el.addClass(shata['active_menu_class']);
             $(document.body).css({overflow: 'hidden'});
         }
     };
@@ -85,21 +106,27 @@
         
         if(window.innerWidth <= upper_break && window.innerWidth > lower_break){
             $(document.body).css({overflow: 'visible'});
-            el
-                .animate(
-                    {
-                        left: '-2000px'
-                    }, 
-                    {
-                        'duration': 400, 
-                        'complete': function(){
-                            el.css({display: 'none', left: 0});
+            if(shata['animate']){
+                el
+                    .animate(
+                        {
+                            left: '-2000px'
+                        }, 
+                        {
+                            'duration': 400, 
+                            'complete': function(){
+                                el
+                                    .removeClass(shata['active_menu_class'])
+                                    .css({left: 0});
+                            }
                         }
-                    }
-                );
+                    );
+            }else{
+                el.removeClass(shata['active_menu_class']);
+            }
         }else if(window.innerWidth <= lower_break){
             $(document.body).css({overflow: 'visible'});
-            el.css({display: 'none'});
+            el.removeClass(shata['active_menu_class']);
         }
     };
     
@@ -112,13 +139,11 @@
             
             var break_width = shata['breakpoints'][shata['partial_breakpoint']];
             if(window.innerWidth <= break_width){
-                $(this).addClass(shata['active_menu_class']);
-                if($(this).css('display') === 'block'){
-                    $(document.body).css({overflow: 'hidden'});
-                }
+                $(this).addClass(shata['sliding_menu_class']);
                 shata['btn_open_object'].css({display: 'inline-block'});
             }else{
                 $(this).removeClass(shata['active_menu_class']);
+                $(this).removeClass(shata['sliding_menu_class']);
                 $(document.body).css({overflow: 'visible'});
                 shata['btn_open_object'].css({display: 'none'});
             }
